5437dfb76a30cf12ba144faeef8bd9f835afb753
Kumar Kaushik <kumarkaushik.tallam@nutanix.com>
2020-05-24 17:17:33 +0530

    added react youtube

--- client/src/SeasonPage.js
+++ client/src/SeasonPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import YouTube from 'react-youtube';
 
 export default class SeasonPage extends Component {
 
   state = {
-    data: {}
+    data: {},
+    mute: false,
   }
 
   componentDidMount = () => {
@@ -21,21 +23,34 @@ export default class SeasonPage extends Component {
     if (!Vid) {
       return null;
     }
+    const playerVars = {
+      controls: 0,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 1,
+      loop: 1,
+      start: 30,
+      playlist: Vid,
+      mute: this.state.mute ? 1 : 0
+    }
+    const opts = {
+      allow: "autoplay; fullscreen",
+      frameBorder: "0",
+      playerVars
+    }
     console.log(Vid);
     return (<div>
     <div className="video-background">
       <div className="video-foreground">
-        <iframe id="video" title="video" src={ `https://www.youtube.com/embed/${Vid}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&start=30&playlist=${Vid}` } frameBorder="0" allow="autoplay; fullscreen"/>
+        <YouTube videoId={ Vid } opts={opts} />
       </div>
     </div>
     <div id="vidtop-content">
       <div className="vid-info">
-          <h1>YouTube Fullscreen Background Demo</h1>
-          <p>The International Space Station orbits the Earth every 92 minutes, with its crew seeing a sunrise 15 times a day. It exists as a scientific, educational, and engineering platform in low orbit, 330 to 435 kilometres above the Earth.</p>
-          <p>Original timelapse by Riccardo Rossi (ISAA), used under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. Raw photos courtesy of http://eol.jsc.nasa.gov/</p>
-        <a href="/500/Use-YouTube-Videos-as-Fullscreen-Web-Page-Backgrounds">Full article</a>
-        </div>
+        <h1>{this.state.data.Place}</h1>
+        {/* <button type="button" className="btn btn-sm btn-outline-secondary">Mute</button> */}
       </div>
+    </div>
     </div>);
   }
 }
